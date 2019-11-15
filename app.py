1959d096b6d89a5ff2743ca33d8367ee7289f472
Firas Moosvi <firas@moosvi.com>
2019-11-14 20:28:20 -0800

    add server = app.server

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import dash_html_components as html
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
