57934fbc352d5e9048a57bf07840f63c1d86d1e5
Firas Moosvi <firas@moosvi.com>
2019-11-28 09:10:46 -0800

    minor update to test.R

--- R/Class App/test.R
+++ R/Class App/test.R
@@ -1,5 +1,5 @@
 print("Congratulations, the R installation on your system appears to be working correctly.")
 
-print("Your version of R is: ")
+print("Your version of R is printed below")
 
 print(version)
